d7739dd141fa40023e16904042f7347d3ef31c30
mathiasdan1 <mathiasdaniel001@gmail.com>
2024-11-19 23:38:47 +0500

    server.js

--- src/lib/server.js
+++ src/lib/server.js
@@ -1,36 +1,75 @@
-import dotenv from "dotenv";
-import createServer from "./createServer.js";
-import colors from "colors";
+import getHandler from "./getHandler.js";
+import httpProxy from "http-proxy";
+import http from "node:http";
 
-dotenv.config();
+export default function createServer(options) {
+  options = options || {};
 
-const host = process.env.HOST || "127.0.0.1";
-const port = process.env.PORT || 8080;
-const web_server_url = process.env.PUBLIC_URL || `http://${host}:${port}`;
+  const httpProxyOptions = {
+    xfwd: true,
+    secure: process.env.NODE_TLS_REJECT_UNAUTHORIZED !== "0",
+  };
 
-export default function server() {
-  createServer({
-    originBlacklist: ["*"],
-    originWhitelist: process.env.ALLOWED_ORIGINS
-      ? process.env.ALLOWED_ORIGINS.split(",")
-      : [],
-    requireHeader: [],
-    removeHeaders: [
-      "cookie",
-      "cookie2",
-      "x-request-start",
-      "x-request-id",
-      "via",
-      "connect-time",
-      "total-route-time",
-    ],
-    redirectSameOrigin: true,
-    httpProxyOptions: {
-      xfwd: false,
-    },
-  }).listen(port, Number(host), function () {
-    console.log(
-      colors.green("Server running on ") + colors.blue(`${web_server_url}`)
+  if (options.httpProxyOptions) {
+    Object.keys(options.httpProxyOptions).forEach(function (option) {
+      httpProxyOptions[option] = options.httpProxyOptions[option];
+    });
+  }
+
+  const proxyServer = httpProxy.createProxyServer(httpProxyOptions);
+  const requestHandler = getHandler(options, proxyServer);
+  let server;
+  const handleCors = (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
     );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+
+    if (req.method === "OPTIONS") {
+      res.writeHead(204);
+      res.end();
+      return true;
+    }
+    return false;
+  };
+
+  if (options.httpsOptions) {
+    server = https.createServer(options.httpsOptions, (req, res) => {
+      if (handleCors(req, res)) return;
+      requestHandler(req, res);
+    });
+  } else {
+    server = http.createServer((req, res) => {
+      if (handleCors(req, res)) return;
+      requestHandler(req, res);
+    });
+  }
+
+  proxyServer.on("error", function (err, req, res) {
+    console.error("Proxy error:", err);
+    if (res.headersSent) {
+      if (!res.writableEnded) {
+        res.end();
+      }
+      return;
+    }
+
+    const headerNames = res.getHeaderNames
+      ? res.getHeaderNames()
+      : Object.keys(res._headers || {});
+    headerNames.forEach(function (name) {
+      res.removeHeader(name);
+    });
+
+    res.writeHead(404, { "Access-Control-Allow-Origin": "*" });
+    res.end("Not found because of proxy error: " + err);
   });
+
+  return server;
 }
